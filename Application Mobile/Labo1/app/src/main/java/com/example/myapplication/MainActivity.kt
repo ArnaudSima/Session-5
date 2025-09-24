2bc68daf37fa9b884e876de387cfcbfca5998169
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-09-23 22:32:15 -0400

    aaa

--- Application Mobile/Labo1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Application Mobile/Labo1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -29,17 +30,22 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlin.math.exp
+
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -57,110 +63,108 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-    var expanded1 by remember { mutableStateOf(false) }
-    var expanded2 by remember { mutableStateOf(false) }
-
-    Scaffold(
-
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Labo 1")
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
-                ),
-                actions = {
-                    Row {
-                        IconButton(
-                            onClick = {
-                                expanded1 = true
-                            },
-                        ) { Icon(Icons.Default.Delete, contentDescription = "delete") }
-
-                        DropdownMenu(
-                            expanded = expanded1,
-                            onDismissRequest = { expanded1 = false }) {
-                            DropdownMenuItem(
-                                text = { Text("Add") },
-                                onClick = {}
-                            )
-                        }
-
-                        IconButton(
-                            onClick = { expanded2 = true },
-
-                            ) {
-                            Icon(Icons.Default.Add, contentDescription = "Ajouter")
-                        }
-                        DropdownMenu(
-                            expanded = expanded2,
-                            onDismissRequest = { expanded2 = false },
-                        ) {
-                            DropdownMenuItem(
-                                text = { Text("Add") },
-                                onClick = {}
-                            )
-                        }
-                    }
-
-
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-
-            ) {
-
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text("Points de vie : ")
-                    Button(onClick = {}
-                    ) { Text(text = "Attaquer") }
-                }
-
+    Scaffold(topBar = {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+        ), title = { Text("Labo 1 ") }, actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    Icons.Default.Delete, contentDescription = "Delete"
+                )
             }
-        }
-
-    )
-
-
-    { padding ->
+            IconButton(onClick = {}) { Icon(Icons.Default.Add, contentDescription = "Add") }
+        })
+    }) { padding ->
         Column(
             modifier = Modifier
                 .padding(padding)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth().weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text("Joueur")
-                Text("90 points de vie")
-                Text("10 forces")
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth().weight(3f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Image(painter = painterResource(R.drawable.caterpie), contentDescription = "pokemon")
-            }
-
+            PokemonGamePlay()
         }
-
     }
 }
 
+@Composable
+fun PokemonGamePlay() {
+    val CaterPie = Pokemon(
+        pointsDeVies = 20, idDrawable = R.drawable.caterpie, force = 10, description = "caterpie"
+    )
+    val Magikarp = Pokemon(
+        pointsDeVies = 50, idDrawable = R.drawable.magikarp, force = 25, description = "magikarp"
+    )
+    val Gyarados = Pokemon(
+        pointsDeVies = 100, idDrawable = R.drawable.gyarados, force = 50, description = "gyarados"
+    )
+    val Pikachu = Pokemon(
+        pointsDeVies = 50, idDrawable = R.drawable.pikachu, force = 25, description = "pikachu"
+    )
+    val list = listOf(CaterPie, Magikarp, Gyarados, Pikachu)
+    var indicePokemonActuel by remember { mutableIntStateOf(0) }
+    var pokemonActuelPointsDeVies by remember { mutableIntStateOf(list[indicePokemonActuel].pointsDeVies) }
+    var pokemonActuelDescription by remember { mutableStateOf(list[indicePokemonActuel].description) }
+    var pokemonActuelIdImage by remember { mutableIntStateOf(list[indicePokemonActuel].idDrawable) }
+    var pokemonActuelForce by remember { mutableIntStateOf(list[indicePokemonActuel].force) }
+    var JoueurPointsDeVies by remember { mutableIntStateOf(100) }
+    var JoueurForce by remember { mutableIntStateOf(10) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 0.dp, 0.dp, 100.dp),
+        verticalArrangement = Arrangement.Center,
+        Alignment.CenterHorizontally
+    ) {
+        Text("Joueur")
+        Text("$JoueurPointsDeVies points de vie")
+        Text("$JoueurForce force")
+
+    }
+    Image(
+        painter = painterResource(id = pokemonActuelIdImage),
+        contentDescription = pokemonActuelDescription
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 100.dp, 0.dp, 0.dp),
+        verticalArrangement = Arrangement.Center,
+        Alignment.CenterHorizontally
+    ) {
+        Text("$pokemonActuelPointsDeVies points de vie")
+        Button(onClick = {
+            pokemonActuelPointsDeVies -= JoueurForce
+            if (pokemonActuelPointsDeVies <= 0) {
+                indicePokemonActuel++
+                pokemonActuelForce = list[indicePokemonActuel].force
+                pokemonActuelDescription = list[indicePokemonActuel].description
+                pokemonActuelIdImage = list[indicePokemonActuel].idDrawable
+                pokemonActuelPointsDeVies = list[indicePokemonActuel].pointsDeVies
+            } else {
+                JoueurPointsDeVies -= pokemonActuelForce
+            }
+            if(JoueurPointsDeVies <= 0){
+                indicePokemonActuel = 0
+                pokemonActuelForce = list[indicePokemonActuel].force
+                pokemonActuelDescription = list[indicePokemonActuel].description
+                pokemonActuelIdImage = list[indicePokemonActuel].idDrawable
+                pokemonActuelPointsDeVies = list[indicePokemonActuel].pointsDeVies
+                JoueurPointsDeVies = 100
+            }
+        }, content = { Text("Attaquer") })
+    }
+}
+
+class Pokemon constructor(
+    val pointsDeVies: Int, val idDrawable: Int, val force: Int, val description: String
+)
+
 @Preview(showBackground = true)
+
 @Composable
 fun GreetingPreview() {
 
