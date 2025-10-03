f473c9e1d35258492a935a05fbcc8e0b9ced6526
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-10-02 23:58:03 -0400

    update

--- Application Mobile/TP1/app/src/main/java/com/example/myapplication/DashBoard.kt
+++ Application Mobile/TP1/app/src/main/java/com/example/myapplication/DashBoard.kt
@@ -1,16 +1,10 @@
 package com.example.myapplication
 
-import android.content.Context
-import android.graphics.drawable.shapes.Shape
-import android.widget.Toast
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,25 +16,21 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -51,24 +41,30 @@ import androidx.navigation.NavController
 fun DashBoard(navController: NavController) {
     val context = LocalContext.current
     var estAffiche by remember { mutableStateOf(false) }
+    var titre by remember { mutableStateOf("") }
+    var kilometrage by remember { mutableIntStateOf(0) }
+    var prix by remember { mutableIntStateOf(0) }
 
     class Vehicule(
-        val modele: String,
+        val id: Int,
+        val titre: String,
         val kilometrage: Int,
         val image: Int,
         val son: Int,
         val prix: Int
     );
-    val listeVehicules = listOf<Vehicule>(
+    var listeVehicules = listOf<Vehicule>(
         Vehicule(
-            modele = "1977 Ford Mustang Shelby",
+            id = 1,
+            titre = "1977 Ford Mustang Shelby",
             kilometrage = 120000,
             image = R.drawable.shelby,
             son = R.raw.shelby_son,
             prix = 12000
         ),
         Vehicule(
-            modele = "1980 Ford Bronco",
+            id = 2,
+            titre = "1980 Ford Bronco",
             kilometrage = 80000,
             image = R.drawable.bronco,
             son = R.raw.bronco_son,
@@ -76,14 +72,16 @@ fun DashBoard(navController: NavController) {
 
             ),
         Vehicule(
-            modele = "1982 chevrolet silverado",
+            id = 3,
+            titre = "1982 chevrolet silverado",
             kilometrage = 120000,
             image = R.drawable.silverado,
             son = R.raw.pick_up_son,
             prix = 10000
         ),
         Vehicule(
-            modele = "1993 shadow vt1100",
+            id = 4,
+            titre = "1993 shadow vt1100",
             kilometrage = 70000,
             image = R.drawable.shadow,
             son = R.raw.pick_up_son,
@@ -92,8 +90,15 @@ fun DashBoard(navController: NavController) {
     )
     LazyColumn {
         items(items = listeVehicules) { vehicule ->
-            Column(modifier = Modifier.clickable(onClick = { estAffiche = true })) {
-                Text(vehicule.modele)
+            Column(modifier = Modifier.clickable(onClick = {
+                estAffiche = true
+                titre = vehicule.titre
+                kilometrage = vehicule.kilometrage
+                prix = vehicule.prix
+                navController.navigate(Ecrans.Modification.route)
+
+            })) {
+                Text(vehicule.titre)
                 Text(vehicule.kilometrage.toString())
                 Image(
                     modifier = Modifier
@@ -101,7 +106,7 @@ fun DashBoard(navController: NavController) {
                         .height(300.dp)
                         .padding(0.dp, 2.dp),
                     painter = painterResource(vehicule.image),
-                    contentDescription = vehicule.modele,
+                    contentDescription = vehicule.titre,
                     contentScale = ContentScale.Crop
                 )
                 IconButton(
@@ -120,24 +125,5 @@ fun DashBoard(navController: NavController) {
 
 
         }
-    }
-    PopUp(estAffiche, onChangeEstAffiche = { estAffiche = it })
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PopUp(estAffiche: Boolean, onChangeEstAffiche: (Boolean) -> Unit) {
-    if (estAffiche) {
-        BasicAlertDialog(onDismissRequest = { onChangeEstAffiche(false) }) {
-            Column (modifier = Modifier
-                .background(color = Color.White, shape = RoundedCornerShape(16.dp))
-                .width(200.dp)
-                .height(400.dp)) {
-
-            }
-        }
-
-    } else {
-        return
     }
 }
--- Application Mobile/TP1/app/src/main/java/com/example/myapplication/Ecrans.kt
+++ Application Mobile/TP1/app/src/main/java/com/example/myapplication/Ecrans.kt
@@ -3,4 +3,5 @@ package com.example.myapplication
 sealed class Ecrans(val route : String){
     object Connexion : Ecrans("connexion")
     object DashBoard : Ecrans("dash_board")
+    object Modification : Ecrans("modification")
 }
